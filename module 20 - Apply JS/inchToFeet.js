97fb4990132a066ca176c93b32edd18b09ed5e7a
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2022-01-29 22:13:20 +0600

    adding new convertion file

--- module 20 - Apply JS/inchToFeet.js
+++ module 20 - Apply JS/inchToFeet.js
@@ -1,14 +1,30 @@
-var inches = 132;
+function inchToFeet(inches) {
+  var feet = inches / 12;
+  return feet;
+}
 
-var feet = inches / 12;
-console.log(feet);
+var myInches = 132;
+var feet = inchToFeet(myInches);
+console.log("myInches in feet", feet);
 
 var dadiInches = 144;
-var feet = dadiInches / 12;
+var feet = inchToFeet(dadiInches);
 console.log(feet);
 
-var naniIches = 156;
-var feet = naniIches / 12;
+var naniInches = 156;
+var feet = inchToFeet(naniInches);
+console.log(feet);
 
 var nanaInches = 168;
-var feet = nanaInches / 12;
+var feet = inchToFeet(nanaInches);
+console.log(feet);
+
+//mile to kilometer
+
+function mileToKilometer(miles) {
+  var km = miles * 1.6093;
+  return km;
+}
+
+var marathon = mileToKilometer(26.2);
+console.log("marthon in km:", marathon);
