7b6c0872415964e305fcc2d0067b46861a4cb11d
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2022-01-30 20:43:48 +0600

    changing few

--- module 20 - Apply JS/factorial-while-loop.js
+++ module 20 - Apply JS/factorial-while-loop.js
@@ -11,16 +11,29 @@
 // const myFactorial = getFactorial(6);
 // console.log(myFactorial);
 
-function getFactorial(number) {
-  let factorial = 1;
-  let i = number;
+// function getFactorial(number) {
+//   let factorial = 1;
+//   let i = number;
 
-  while (i >= 1) {
+//   while (i >= 1) {
+//     factorial = factorial * i;
+//     i--;
+//   }
+//   return factorial;
+// }
+
+// const myFactorial = getFactorial(5);
+// console.log(myFactorial);
+
+//for loop reverse
+
+function getFactorial2(number) {
+  let factorial = 1;
+  for (let i = number; i >= i; i--) {
     factorial = factorial * i;
-    i--;
   }
   return factorial;
 }
 
-const myFactorial = getFactorial(5);
-console.log(myFactorial);
+const myFactorial2 = getFactorial2(6);
+console.log(myFactorial2);
