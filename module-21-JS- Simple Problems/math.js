62916f0faeae634edeef15798dd5dd8c5222c7fe
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2022-01-31 21:47:18 +0600

    math random file

---
+++ module-21-JS- Simple Problems/math.js
@@ -0,0 +1,14 @@
+// const myNumber = -5;
+// const output = Math.abs(myNumber);
+
+const myNumber = 21.70001;
+
+// const output = Math.ceil(myNumber);
+
+// const output = Math.floor(myNumber);
+
+// const output = Math.round(myNumber);
+const output = Math.random() * 6;
+
+const rounded = Math.ceil(output);
+console.log(rounded);
